3f00d12b8efca237679e45ef8eb7fe86fc4f5a11
arpantopno <arpan.sharon@gmail.com>
2018-06-18 09:51:01 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!Doctype HTML>
+<html>
+<head>
+<title>PHP FORM</title>
+</head>
+<body>
+<form action="next.php" method="post">
+<table>
+<tr><td>Enter Name:</td><td><input type="text" name="name"></td></tr>
+<tr><td>Enter DOB:</td><td><input type="date" name="date"></td><tr>
+<tr><td>Enter address:</td><td><textarea name="address"></textarea></td><tr>
+<tr><td>Enter Email:</td><td><input type="email" name="email"></td><tr>
+<tr><td colspan="2"><input type="submit"></td></tr>
+</form>
+</body>
+</html>
